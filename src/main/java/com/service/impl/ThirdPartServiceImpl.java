107c7932a1a2a36873049048c1bea0db5256aede
caixu23 <caixu23@gmail.com>
2014-07-04 12:31:15 +0800

    -----

--- src/main/java/com/service/impl/ThirdPartServiceImpl.java
+++ src/main/java/com/service/impl/ThirdPartServiceImpl.java
@@ -3,27 +3,31 @@ package com.service.impl;
 import com.domainVO.ThirdPartRequestVO;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.apache.log4j.Logger;
 
 import javax.annotation.Resource;
+
 
 /**
  * Created by chace.cai on 2014/6/30.
  */
 @Service("thirdPartServiceImpl")
 public class ThirdPartServiceImpl implements com.service.ThirdPartService {
+     Logger logger = Logger.getLogger(ThirdPartServiceImpl.class);
     @Resource
     private JdbcTemplate jdbcTemplate;
 
     @Override
     public String setThirdPartMessage(ThirdPartRequestVO thirdPartMessage){
+        logger.error("内容:"+thirdPartMessage.getBstrMessageContent()+"--bucode:"+thirdPartMessage.getBstrBusinessCode()+"--messageid"+thirdPartMessage.getBstrMoMessageID());
         if(thirdPartMessage==null || thirdPartMessage.getBstrMoMessageID()==null || thirdPartMessage.getBstrMessageContent()==null
                 || thirdPartMessage.getBstrMessageContent().length()>200){
             return "";
         }
-        String sql="update sms_send_tb set msg = ?,flag='0' where message_id=? and flag = '9'";
-        Object[] parm=new Object[]{thirdPartMessage.getBstrMessageContent(),thirdPartMessage.getBstrMoMessageID()};
+        String sql="update sms_send_tb set msg = ?,flag='1',business_code=? where message_id=? and flag = '9'";
+        Object[] parm=new Object[]{thirdPartMessage.getBstrMessageContent(),thirdPartMessage.getBstrBusinessCode(),thirdPartMessage.getBstrMoMessageID()};
         //int[] types = new int[]{Types.INTEGER,Types.VARCHAR,Types.CHAR,Types.VARCHAR};
-        //jdbcTemplate.update(sql,parm);
+        jdbcTemplate.update(sql,parm);
         return getResponseXml(thirdPartMessage.getBstrMoMessageID());
     }
 
